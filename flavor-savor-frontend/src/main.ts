8a3271d2b3715e30d5bf8124f69821494944cbe1
Emil Auer <e.auer@itkaufmann.at>
2023-03-28 21:47:42 +0200

    Import primevue

--- flavor-savor-frontend/src/main.ts
+++ flavor-savor-frontend/src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config';
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
@@ -6,9 +7,19 @@ import router from './router'
 
 import './assets/main.css'
 
+//theme
+import "primevue/resources/themes/lara-light-indigo/theme.css";
+
+//core
+import "primevue/resources/primevue.min.css";
+
+//icons
+import "primeicons/primeicons.css";
+
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(PrimeVue)
 app.use(router)
 
 app.mount('#app')
